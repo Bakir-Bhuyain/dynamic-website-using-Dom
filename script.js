a611c09b499a887d6c8a60160eb64e6780c6c88c
Bakir-Bhuyain <hemalbakir@gmail.com>
2026-01-21 14:21:40 +0600

    using dom after clicking a button the user active status updated

--- script.js
+++ script.js
@@ -6,10 +6,24 @@
 // const change = document.getElementById("play");
 // change.innerText = "Code Worked!";
 
+const changeName = document.getElementById("myName");
+const button = document.getElementById("btn");
+button.addEventListener("click", function () {
+  changeName.innerText = "i am a junior developer";
+  changeName.style.color = "blue";
+});
 
-const changeName = document.getElementById('btn');
-const button = document.getElementById('myName');
-button.addEventListener('click', function(){
-      changeName.innerText = 'i am a junior developer';
-      changeName.style.color= "blue";     
-})
+let isOnline = false;
+const server = document.getElementById("server-status");
+const button2 = document.getElementById("btn2");
+button2.addEventListener("click", function () {
+  if (isOnline === false) {
+    server.innerText = "system online";
+    server.style.color = "green";
+    isOnline = true;
+  } else{
+    server.innerText = "system offline";
+    server.style.color = "red";
+    isOnline = false;
+  }
+});
